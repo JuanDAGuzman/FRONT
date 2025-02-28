badd42ce63a28690eff1fab400be9f0793c63352
Camilo Gomez <camigomezbernal@gmail.com>
2025-02-28 12:14:14 -0500

    Se arregla login, se cambia components por pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import Login from './components/Login/login' //Se importa el componente Login
+import Login from './pages/Login/login' //Se importa el componente Login
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <Login /> {/* Se usa el componente de Login */}
+      <Login /> {/* Se usa el componente de login */}
     </div>
   )
 }
