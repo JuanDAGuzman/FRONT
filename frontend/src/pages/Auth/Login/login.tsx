b2dc6b5cf63407f131f07e39fe69e81b9f4666f6
Camilo Gomez <camigomezbernal@gmail.com>
2025-03-01 18:52:09 -0500

    Se organizan vistas

--- frontend/src/pages/Auth/Login/login.tsx
+++ frontend/src/pages/Auth/Login/login.tsx
@@ -5,27 +5,30 @@ import { useNavigate } from "react-router-dom";
 const Login: React.FC = () => {
     const navigate = useNavigate()
     return (
-        <div className={styles.fondo}>
 
-            <div className={styles.container}>
-                <div className={styles.form}>
-                    <h2 className={styles.title}>Iniciar Sesión</h2>
-                    <form>
-                        <div className={styles.input_box}>
-                            <input type="email" placeholder='Email Andress' required/>
-                        </div>
-                        <div className={styles.input_box}>
-                            <input type="password" placeholder='Password' required/>
-                        </div>
-                        <button type='submit' className={styles.btn} onClick={() => navigate("/")}>Sign In</button>
-                    </form>
+        <div className={styles.login}>
+            <div className={styles.fondo}>
+
+                <div className={styles.container}>
+                    <div className={styles.form}>
+                        <h2 className={styles.title}>Iniciar Sesión</h2>
+                        <form>
+                            <div className={styles.input_box}>
+                                <input type="email" placeholder='Email Andress' required/>
+                            </div>
+                            <div className={styles.input_box}>
+                                <input type="password" placeholder='Password' required/>
+                            </div>
+                            <button type='submit' className={styles.btn} onClick={() => navigate("/")}>Sign In</button>
+                        </form>
+                    </div>
+
+                    <div className={styles.logo_UMB}>
+                        <img src="public/umb-logo.png" alt="UMB logo" />
+                    </div>
                 </div>
 
-                <div className={styles.logo_UMB}>
-                    <img src="public/umb-logo.png" alt="UMB logo" />
-                </div>
             </div>
-
         </div>
     )
 }
