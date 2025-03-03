7e38b92182152ddaddd05a5fbcb218f310bb5258
Juan Arevalo <diegoarevalo.jg@academia.umb.edu.co>
2025-03-03 17:28:56 -0500

    :memo: add:styles and new structure

--- frontend/src/pages/Home/Hero/Hero.tsx
+++ frontend/src/pages/Home/Hero/Hero.tsx
@@ -5,23 +5,50 @@ import Logo from "../../../assets/logo2.png";
 const Hero: React.FC = () => {
   return (
     <section className="mt-36 text-white">
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div>Texto</div>
-        <img src={Logo} alt="Logo2-app" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          accusamus nihil nulla inventore, laudantium quaerat delectus ut
-          corrupti recusandae vero placeat officiis repudiandae exercitationem
-          voluptatibus sunt enim. In laudantium dolores similique amet quasi
-          optio, odit facere sapiente est expedita, corporis quod molestiae
-          alias. Natus magnam quibusdam beatae voluptatibus provident
-          laudantium.
-        </p>
-        <div className="flex justify-center gap-4">
-          <a>Unete Ahora <i className="bi bi-arrow-right-square-fill"></i></a>
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center px-8 py-4">
+        {/* Columna izquierda: Logo + Texto */}
+        <div className="flex flex-col items-center text-center gap-y-4">
+          <img className="w-[400px] md:w-[500px]" src={Logo} alt="Logo2-app" />
+
+          <p className="text-black px-4 py-2 max-w-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+            accusamus nihil nulla inventore, laudantium quaerat delectus ut
+            corrupti recusandae vero placeat officiis repudiandae exercitationem
+            voluptatibus sunt enim. In laudantium dolores similique amet quasi
+            optio, odit facere sapiente est expedita, corporis quod molestiae
+            alias. Natus magnam quibusdam beatae voluptatibus provident
+            laudantium.
+          </p>
+
+          <div className="flex justify-center gap-4">
+            <a href="#" className="text-blue-600 flex items-center gap-2">
+              Únete Ahora <i className="bi bi-arrow-right-square-fill"></i>
+            </a>
+          </div>
+
+          <p className="text-black px-4 py-2 max-w-2xl">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad odio
+            suscipit architecto. Aspernatur, dolore? Placeat, laudantium nulla!
+            Dolore qui deleniti molestiae fuga dolorum velit eius vitae, a quasi
+            quaerat cumque quod atque maxime rerum possimus minus quas tempore
+            nulla, repellat esse explicabo? Officiis, asperiores expedita
+            aliquid doloribus quas libero autem?
+          </p>
+
+          <div className="flex justify-center gap-4">
+            <a href="#" className="text-blue-600 flex items-center gap-2">
+              Saber más <i className="bi bi-arrow-right-square-fill"></i>
+            </a>
+          </div>
         </div>
-        <div>
-          <img src={Personaje} alt="Personaje-app" />
+
+        {/* Columna derecha: Imagen del personaje */}
+        <div className="flex justify-center">
+          <img
+            className="max-w-xs md:max-w-md"
+            src={Personaje}
+            alt="Personaje-app"
+          />
         </div>
       </div>
     </section>
--- frontend/src/pages/Home/home.tsx
+++ frontend/src/pages/Home/home.tsx
@@ -2,22 +2,23 @@ import React from "react";
 import Navbar from "./NavBar/Navbar";
 import Hero from "./Hero/Hero";
 import "./home_module.css";
-import Fondo from "../../assets/prueba.png";
+// import Fondo from "../../assets/prueba.png";
 
 const Home: React.FC = () => {
-  const bgImagen = {
-    backgroundImage: `url(${Fondo})`,
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "bottom",
-    backgroundSize: "cover",
-    position: "relative",
-  };
+  // const bgImagen = {
+  //   backgroundImage: `url(${Fondo})`,
+  //   backgroundRepeat: "no-repeat",
+  //   backgroundPosition: "bottom",
+  //   backgroundSize: "cover",
+  //   position: "relative",
+  // };
 
   return (
-    <div style={bgImagen} className="overflow-hidden min-h-screen">
+    <div>
       <Navbar />
       <Hero />
     </div>
+    //  style={bgImagen} className="overflow-hidden min-h-screen">
   );
 };
 
