43e17e7a6581a3b331b7e629823eadb9a61f6111
Juan Arevalo <diegoarevalo.jg@academia.umb.edu.co>
2025-03-04 11:21:56 -0500

    :memo: fixed & add: links of linkedin in Us page

--- frontend/src/pages/Us/Hero/HeroUs.tsx
+++ frontend/src/pages/Us/Hero/HeroUs.tsx
@@ -3,16 +3,19 @@ import React from "react";
 const developers = [
   {
     name: "Juan Diego Arevalo",
+    linkedin: "https://www.linkedin.com/in/juanagdev",
     github: "https://github.com/JuanDAGuzman",
     info: "Ingeniero en Desarrollo de Software | Desarrollo Web y Backend | Integración de sistemas, bases de datos y desarrollo de APIs",
   },
   {
     name: "Camilo Andres Gomez",
+    linkedin: "https://www.linkedin.com/in/camilogdev/",
     github: "https://github.com/CamAGomezB27",
     info: "Ingeniero en Desarrollo de Software en formación | Desarrollo Web, Bases de Datos & BI | Dominio en JavaScript, Python, HTML, CSS, MySQL | Enfocado en Soluciones Innovadoras",
   },
   {
     name: "Felipe Cruz",
+    linkedin: "https://www.linkedin.com/in/felipe-cruz-351115213",
     github: "https://github.com/carlosramirez",
     info: "Desarrollador Backend enfocado en APIs con Express y MongoDB.",
   },
@@ -31,13 +34,25 @@ const HeroUs: React.FC = () => {
             <i className="bi bi-github text-4xl text-gray-900"></i>
             <h2 className="text-xl font-semibold mt-2">{dev.name}</h2>
             <p className="text-gray-600 text-sm mt-2">{dev.info}</p>
+            {/* Enlace de GitHub */}
+
             <a
+            
               href={dev.github}
               target="_blank"
               rel="noopener noreferrer"
               className="mt-4 inline-block text-[#B71C1C]  font-semibold transition-transform hover:scale-110 duration-300"
             >
               Ver perfil en GitHub
+            </a>
+            {/* Enlace de LinkedIn */}
+            <a
+              href={dev.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-2 inline-block text-[#0077B5] font-semibold transition-transform hover:scale-110 duration-300"
+            >
+              Ver perfil de LinkedIn
             </a>
           </div>
         ))}
