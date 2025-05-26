9b813369783b1a242fc693427e00f5f70e641510
Camilo Gomez <camigomezbernal@gmail.com>
2025-05-26 17:27:37 -0500

    Se crea formulario de agendamiento para citas

--- frontend/src/pages/Management/Doctor_M/Agendar_CItas_Doctor/agendar_citas_Doc.tsx
+++ frontend/src/pages/Management/Doctor_M/Agendar_CItas_Doctor/agendar_citas_Doc.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './agendar_citas_Doc.module.css';
 import { useNavigate } from 'react-router-dom';
 import Navbar_G from '../../../../components/NavBars/Navbar_Globla';
 
 const agendarcita_doc: React.FC = () => {
   const navigate = useNavigate();
+  const [cedulaPaciente, setCedulaPaciente] = useState('');
+  const [fecha, setFecha] = useState('');
+  const { hora, setHora } = useState('');
+  const [motivo, setMotivo] = useState('');
+  const [tipo, setTipo] = useState<'Presencial' | 'Virtual'>('Presencial');
+
   return (
-    <div className={styles.List_Patients_Doc}>
+    <div className={styles.agendar_citas_Doc}>
       <Navbar_G
         profileText="Regresar"
         profilePath="/dashboard_doctor"
@@ -23,6 +29,22 @@ const agendarcita_doc: React.FC = () => {
         alt="Logo medicos"
         className={styles.logo}
       />
+
+      <div className={styles.container}>
+        {/* FORMULARIO PARA AGENDAR CITA */}
+        <section className={styles.form}>
+          <h2>Agendar Nueva Cita</h2>
+          <div className={styles.group}>
+            <label htmlFor="">Cedula del Paciente</label>
+            <input
+              type="text"
+              value={cedulaPaciente}
+              onChange={e => setCedulaPaciente(e.target.value)}
+              placeholder="Buscar por Cédula"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
