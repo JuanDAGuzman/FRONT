ad01c3f155798fef9ab8bc46393d2fda8627aec5
Juan Diego Arévalo Guzmán <diegoarevalo.jg@academia.umb.edu.co>
2025-09-04 10:27:25 -0500

    changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  
-  base: '/', // Asegura que las rutas sean correctas
-  server: {
-    open: true, // Opcional: abre el navegador automáticamente
+  plugins: [react(), tailwindcss()],
+  css: {
+    // Evita Lightning CSS para los transform (usa PostCSS/Tailwind)
+    transformer: 'postcss',
   },
+  base: '/',
+  server: { open: true },
   build: {
     outDir: 'dist',
+    // Minificador de CSS: usa esbuild (no lightningcss)
+    cssMinify: 'esbuild',
   },
 })
